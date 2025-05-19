642b4c6dfcb41a2f4b0cea35e76ba4b560d8fbbb
venkatesh <venkateshvenky626@gmail.com>
2025-05-19 18:01:30 +0530

    first commit

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 component=$1
 environment=$2 #dont use env here, it is reserved in linux
+app_version=$3
 #yum install python3.11-devel python3.11-pip -y
 yum install -y ansible python3-dnf
 pip3 install ansible botocore boto3
-ansible-pull -U https://github.com/Venkatesh7422/roboshop-ansible-roles-tf.git -e component=$component -e env=$environment main-tf.yaml
+ansible-pull -U https://github.com/Venkatesh7422/roboshop-ansible-roles-tf.git -e component=$component -e env=$environment -e app_version=$app_version main-tf.yaml
 
 
 #------------------------------------------------------------
